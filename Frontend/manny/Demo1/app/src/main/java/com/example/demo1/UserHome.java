0ea1687319e11a17a1168a6e72edc0bb548282c8
epaz <epaz@iastate.edu>
2021-09-20 12:08:35 -0500

    added settings button

---
+++ Frontend/manny/Demo1/app/src/main/java/com/example/demo1/UserHome.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}
