e4b248ca734edb385289dce5216697430add21f0
epaz <epaz@iastate.edu>
2021-09-27 11:47:22 -0500

    started settings page with new design

--- Frontend/manny/Demo1/app/src/main/java/com/example/demo1/Settings.java
+++ Frontend/manny/Demo1/app/src/main/java/com/example/demo1/Settings.java
@@ -5,16 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Settings extends AppCompatActivity {
-    private Button back;
+    private ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_screen);
-        back = (Button) findViewById(R.id.settings_back);
+        back = (ImageButton) findViewById(R.id.settings_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
