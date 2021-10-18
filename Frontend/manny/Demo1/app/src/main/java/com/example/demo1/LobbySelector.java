a30652d0296e58c1f77c7d7efdd251db2e5169a7
Caden <lastcaden@gmail.com>
2021-10-18 16:12:53 -0500

    finished lobby screen basics

--- Frontend/manny/Demo1/app/src/main/java/com/example/demo1/LobbySelector.java
+++ Frontend/manny/Demo1/app/src/main/java/com/example/demo1/LobbySelector.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,21 +13,23 @@ import java.util.ArrayList;
 
 public class LobbySelector extends AppCompatActivity {
     private ScrollView scroll;
+    private LinearLayout layout;
     ArrayList<Button> al = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.lobby_selector_screen);
+        layout = findViewById(R.id.linear);
         scroll = findViewById(R.id.scroll);
 
-        al.add(new Button(this.getApplicationContext()));
-        al.add(new Button(this.getApplicationContext()));
-        al.add(new Button(this.getApplicationContext()));
-
+        for(int i=0; i<10; i++) {
+            Button button = new Button(this.getApplicationContext());
+            button.setText("" + i);
+            layout.addView(button);
+        }
 
         //TODO api call
 
-        //scroll.addChildrenForAccessibility(new ArrayList<View>(al));
 
     }
 }
