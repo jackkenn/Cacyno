718a5bcbe4c9c6e69a7ac7e7e476b90cc90a9586
Ben Stroup <56928392+bstroup3@users.noreply.github.com>
2021-10-22 18:10:55 -0500

    Added Game and Players controller but still working on foreign keys and primary keys in them

---
+++ Backend/src/main/java/com/_2_ug_1/cacyno/players/IPlayersRepo.java
@@ -0,0 +1,9 @@
+package com._2_ug_1.cacyno.players;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IPlayersRepo extends JpaRepository<Players, String>{
+}
--- SQL_Scripts/CreateTables.sql
+++ SQL_Scripts/CreateTables.sql
@@ -1,6 +1,6 @@
 start transaction;
 
-create user if not exists 'admin'@'localhost' identified by 'password';
+create user if not exists 'admin'@'localhost' identified by 'password!';
 grant all privileges on *.* to 'admin'@'localhost';
 
 use cacyno;
@@ -9,47 +9,24 @@ create table game(
 id varchar(50) NOT NULL,
 user_id varchar(10) NOT NULL,
 isSpectator boolean,
+public_card1 int,
+public_card2 int,
+public_card3 int,
+public_card4 int,
+public_card5 int,
+pot int,
 primary key (id),
 foreign key (user_id) references user(id) ON DELETE CASCADE
-);
-create table current_games_list(
-game_id varchar(50),
-primary key(game_id),
-foreign key(game_id) references game(id) ON DELETE CASCADE
 );
 
 create table players(
 game_id varchar(50), 
 user_id varchar(50),
 current_game_money int,
-card1_num int, 
-card1_suit int,
-card2_num int, 
-card2_suit int,
+card1 int,
+card2 int,
 folded boolean,
+primary key (user_id, game_id),
 foreign key (user_id) references user(id)ON DELETE CASCADE,
-foreign key (game_id) references game(id) ON DELETE CASCADE
-);
-
-create table deck(
-game_id varchar(50),
-cards_num int,
-cards_suit int,
-foreign key (game_id) references game(id)
-);
-
-create table public_cards(
-game_id varchar(50),
-card1_num int, 
-card1_suit int, 
-card2_num int, 
-card2_suit int,
-card3_num int, 
-card3_suit int,
-card4_num int, 
-card4_suit int,
-card5_num int, 
-card5_suit int,
-pot int, 
 foreign key (game_id) references game(id) ON DELETE CASCADE
 );
