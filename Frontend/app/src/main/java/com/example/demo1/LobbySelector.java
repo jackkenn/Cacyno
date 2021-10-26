a0b7f3f77bd8fea810397274c82f1e818a2731e9
Caden <lastcaden@gmail.com>
2021-10-26 00:31:13 -0500

    added functionality to refresh button and back button

--- Frontend/app/src/main/java/com/example/demo1/LobbySelector.java
+++ Frontend/app/src/main/java/com/example/demo1/LobbySelector.java
@@ -16,6 +16,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.gson.annotations.SerializedName;
 import interfaces.ServerCallback;
 import org.json.JSONArray;
@@ -26,12 +27,19 @@ import java.util.ArrayList;
 public class LobbySelector extends AppCompatActivity{
     private ScrollView scroll;
     private LinearLayout layout;
+    private ConstraintLayout constraintLayout;
     ArrayList<Lobby> list;
-    //@RequiresApi(api = Build.VERSION_CODES.O)
+    private ImageButton refresh;
+    private ImageButton back;
+    ArrayList<Lobby> lobbies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lobby_selector_screen);
+        constraintLayout = findViewById(R.id.constraintLayout);
+        back = findViewById(R.id.back);
+        refresh = constraintLayout.findViewById(R.id.refresh);
         Lobby lb = new Lobby();
         list = new ArrayList<>();
         lb.calltoServer(this, list, new ServerCallback(){
@@ -45,6 +53,34 @@ public class LobbySelector extends AppCompatActivity{
         });
         layout = findViewById(R.id.linear);
         scroll = findViewById(R.id.scroll);
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                layout.removeAllViews();
+                list.removeAll(list);
+                lb.calltoServer(view.getContext(), list, new ServerCallback(){
+                    @Override
+                    public void onSuccess(JSONArray response){
+                        for(Lobby i : list) {
+                            View row = getLayoutInflater().inflate(R.layout.lobby_row, layout);
+                        }
+                    }
+                });
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LobbySelector.this, UserHome.class));
+            }
+        });
     }
+
+
+
+
 
 }
