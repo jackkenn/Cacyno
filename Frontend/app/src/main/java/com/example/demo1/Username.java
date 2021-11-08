ad1eb7d005e8351a8206ea8702f8bbb10440c2ff
Caden <lastcaden@gmail.com>
2021-11-07 18:11:51 -0600

    javadocs

--- Frontend/app/src/main/java/com/example/demo1/Username.java
+++ Frontend/app/src/main/java/com/example/demo1/Username.java
@@ -3,7 +3,6 @@ package com.example.demo1;
 import Models.User;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
