143a4a711bf0d29f9cdf0bd04ab41066f929c600
epaz <epaz@iastate.edu>
2021-11-29 16:39:17 -0600

    started web sockets

--- Frontend/app/src/main/java/com/example/demo1/GameScreen.java
+++ Frontend/app/src/main/java/com/example/demo1/GameScreen.java
@@ -4,16 +4,19 @@ import Models.User;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewManager;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import interfaces.IUser;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Objects;
 import java.util.Random;
 
@@ -24,7 +27,7 @@ public class GameScreen extends AppCompatActivity {
     private ImageButton backout;
     private ImageButton chat;
     private ConstraintLayout gameScreen;
-
+    private WebSocketClient mWebSocketClient;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -72,8 +75,13 @@ public class GameScreen extends AppCompatActivity {
         chat.setOnClickListener(v -> {
             View chatview = getLayoutInflater().inflate(R.layout.chat_view, gameScreen);
             chatview.bringToFront();
-            ImageButton x = findViewById(R.id.x_out_chat);
 
+            //buttons
+            ImageButton x = findViewById(R.id.x_out_chat);
+            ImageButton send = findViewById(R.id.send_message);
+            TextView message = findViewById(R.id.message_type);
+
+            //exit out
             x.setOnClickListener(v1 -> {
                 gameScreen.removeView(findViewById(R.id.chat_view_remove));
                 gameScreen.invalidate();
@@ -81,4 +89,44 @@ public class GameScreen extends AppCompatActivity {
 
         });
     }
+
+    private void connectWebSocket() throws URISyntaxException {
+        URI url;
+        /*
+         * To test the clientside without the backend, simply connect to an echo server such as:
+         *  "ws://echo.websocket.org"
+         */
+        url = new URI("ws://10.0.2.2:8080/example"); // 10.0.2.2 = localhost
+        // uri = new URI("ws://echo.websocket.org");
+
+        mWebSocketClient = new WebSocketClient(url) {
+
+
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                System.out.println("connected");
+            }
+
+            @Override
+            public void onMessage(String msg) {
+                Log.i("Websocket", "Message Received");
+                // Appends the message received to the previous messages
+                System.out.println(msg);
+            }
+
+            @Override
+            public void onClose(int errorCode, String reason, boolean remote) {
+                Log.i("Websocket", "Closed " + reason);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+
+
 }
