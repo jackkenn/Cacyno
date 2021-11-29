40368fa9651aa3e962152baacbef27a205b4682f
Caden <lastcaden@gmail.com>
2021-11-29 11:41:52 -0600

    finished HandChecker and the comparator

--- Backend/src/main/java/com/_2_ug_1/cacyno/game/HandChecker.java
+++ Backend/src/main/java/com/_2_ug_1/cacyno/game/HandChecker.java
@@ -11,11 +11,11 @@ public class HandChecker {
 
 
     public static void main(String args[]) {
-        Hand hand = getHandRank(new int[]{2, 3, 4, 5, 10, 7, 8});
+        Hand hand = getHandRank(new int[]{2, 3, 4, 5, 7, 8, 9});
         int[] s = hand.getRanks();
         System.out.println(hand.getHand());
-        for(int i=0; i<s.length; i++) {
-            System.out.println(s[i]);
+        for (int j : s) {
+            System.out.println(j);
         }
     }
 
@@ -86,18 +86,18 @@ public class HandChecker {
                     }
                 }
             }
-        } catch (IndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException ignored) {
 
         }
 
-        for (int i = 0; i <= 2; i++) {
-            for (int j = i + 1; j <= 3; j++) {
-                for (int k = j + 1; k <= 4; k++) {
-                    for (int l = k + 1; l <= 5; l++) {
-                        for (int m = l + 1; m <= 6; m++) {
-                            Card[] temp = new Card[]{cards[i], cards[j], cards[k], cards[l], cards[m]};
+        for (int i = 6; i >=4; i--) {
+            for (int j = i - 1; j >= 3; j--) {
+                for (int k = j - 1; k >=2; k--) {
+                    for (int l = k - 1; l >=1; l--) {
+                        for (int m = l - 1; m >=0; m--) {
+                            Card[] temp = new Card[]{cards[m], cards[l], cards[k], cards[j], cards[i]};
                             if (temp[0].getSuit() == temp[1].getSuit() && temp[0].getSuit() == temp[2].getSuit() && temp[0].getSuit() == temp[3].getSuit() && temp[0].getSuit() == temp[4].getSuit()) {
-                              more then 5 card flush return new Hand(Hands.FLUSH, new int[]{temp[4].getRank(), temp[3].getRank(), temp[2].getRank(), temp[1].getRank(), temp[0].getRank()});
+                              return new Hand(Hands.FLUSH, new int[]{temp[4].getRank(), temp[3].getRank(), temp[2].getRank(), temp[1].getRank(), temp[0].getRank()});
                             }
                         }
                     }
@@ -105,17 +105,16 @@ public class HandChecker {
             }
         }
 
-        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Integer> tempStraightHand = new ArrayList<>();
         for (int i = 0; i <= 6; i++) {
-            if (!temp.contains(ranks[i])) {
-                temp.add(ranks[i]);
+            if (!tempStraightHand.contains(ranks[i])) {
+                tempStraightHand.add(ranks[i]);
             }
         }
-        Collections.sort(temp);
 
-        for (int i = 0; i <= temp.size() - 5; i++) {
-            if (temp.get(i) + 4 == temp.get(i + 4) || (temp.contains(13) && temp.contains(1) && temp.contains(2) && temp.contains(3) && temp.contains(4))) {
-                more than 5 card straight return new Hand(Hands.STRAIGHT, new int[]{temp.get(i + 4)});
+        for (int i = tempStraightHand.size()-5; i >=0; i++) {
+            if (tempStraightHand.get(i) + 4 == tempStraightHand.get(i + 4) || (tempStraightHand.contains(13) && tempStraightHand.contains(1) && tempStraightHand.contains(2) && tempStraightHand.contains(3) && tempStraightHand.contains(4))) {
+                return new Hand(Hands.STRAIGHT, new int[]{tempStraightHand.get(i + 4)});
             }
         }
 
@@ -139,7 +138,7 @@ public class HandChecker {
         for (int i = 0; i < 4; i++) {
             for (int j = i + 1; j < 6; j++) {
                 if (ranks[i] == ranks[i + 1] && ranks[j] == ranks[j + 1]) {
-                    for (int k = ranks.length-1; k >=0; k++) {
+                    for (int k = ranks.length-1; k >=0; k--) {
                         if (k != i && k != i + 1 && k != j && k != j + 1) {
                             if (ranks[i] > ranks[j]) {
                                 return new Hand(Hands.TWOPAIR, new int[]{ranks[i], ranks[i + 1], ranks[j], ranks[j + 1], ranks[k]});
@@ -173,8 +172,8 @@ public class HandChecker {
     }
 
     private static boolean contains(int[] cardsParam, int num) {
-        for (int i = 0; i < cardsParam.length; i++) {
-            if (cardsParam[i] == num) {
+        for (int j : cardsParam) {
+            if (j == num) {
                 return true;
             }
         }
