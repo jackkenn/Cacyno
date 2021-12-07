dfd497cd686625831bf6cf90c887ad08d56eda8b
Ben Stroup <56928392+bstroup3@users.noreply.github.com>
2021-12-06 22:32:59 -0600

    fixed HandComparator

--- Backend/src/main/java/com/_2_ug_1/cacyno/game/HandComparator.java
+++ Backend/src/main/java/com/_2_ug_1/cacyno/game/HandComparator.java
@@ -28,8 +28,9 @@ public class HandComparator {
         int[] hand1Ranks = hand1.getRanks();
         int[] hand2Ranks = hand2.getRanks();
 
+
         if (HandMap.get(hand1.getHand()) != HandMap.get(hand2.getHand())) {
-            return HandMap.get(hand1) - HandMap.get(hand2);
+            return HandMap.get(hand1.getHand()) - HandMap.get(hand2.getHand());
         }
 
         for (int i = 0; i < hand1Ranks.length; i++) {
